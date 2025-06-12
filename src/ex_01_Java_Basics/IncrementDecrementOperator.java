c80d4ccad31bdadca748573ff61311aae1a66395
snehakb999 <snehakbcs@gmail.com>
2025-06-12 17:06:23 +0530

    task 1

---
+++ src/ex_01_Java_Basics/IncrementDecrementOperator.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class IncrementDecrementOperator {
+    public static void main(String[] args) {
+        int i = 11;
+        System.out.println(i++ + ++i);
+        //here 11+13=24
+        
+    }
+}
